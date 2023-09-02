39cf369b461ec280b150ffe30c0d86b1ce02e3ec
Sergei Zuev <zuev56@ro.ru>
2023-09-02 12:36:24 +0300

    fix: Change IBotClient.SendMessageAsync

--- src/Messaging/IBotClient.cs
+++ src/Messaging/IBotClient.cs
@@ -8,7 +8,7 @@ namespace Zs.Bot.Services.Messaging;
 public interface IBotClient
 {
     void AddToMessagePipeline(PipelineStep nextStep, CancellationToken cancellationToken = default);
-    Task<Message> SendMessageAsync(string text, Chat chat, long? replyToMessageId = null, CancellationToken cancellationToken = default);
+    Task<Message> SendMessageAsync(string text, Chat chat, Message? messageToReply = null, CancellationToken cancellationToken = default);
     Task DeleteMessageAsync(Message message, CancellationToken cancellationToken = default);
     Task<string> GetBotInfoAsync(CancellationToken cancellationToken = default);
 }
